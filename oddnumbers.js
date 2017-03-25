e626ff4667459f6608d1b93907880bb91f3f6090
Emily Andrews <emilykandrews@gmail.com>
2017-03-25 10:37:39 -0400

    Add modified code

--- oddnumbers.js
+++ oddnumbers.js
@@ -11,3 +11,21 @@ var myArray = [];
 // Only change code below this line.
 
 
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+for (var i = 1; i < 9; i += 1) {
+  ourArray.push(i);
+}
+
+// Only change code below this line.
+
+
+
